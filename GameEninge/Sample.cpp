5a81f854058f57370353eecf5c7488614e574e9f
Milo <mpmpas1@avans.nl>
2022-12-26 14:21:30 +0100

    Add comments

--- GameEninge/Sample.cpp
+++ GameEninge/Sample.cpp
@@ -34,7 +34,6 @@ void Sample::Play(const bool looping, float volume)
         handlers[temp] = this;
         mutex_.unlock();
 
-        SetVolume(volume);
         running = true;
     }
 }
@@ -50,8 +49,6 @@ void Sample::Play(const int times, float volume)
     
     mutex_.unlock();
 
-    SetVolume(volume);
-
     running = true;
 }
 
@@ -63,6 +60,7 @@ constexpr int ConvertVolume(const float volume, const float begin, const float e
 
 void Sample::SetVolume(const float volume)
 {
+    std::cout << std::to_string(volume) << std::endl;
     Mix_Volume(std::get<0>(channels.back())
         , ConvertVolume(volume, spic::internal::defaults::VOLUME_BEGIN, spic::internal::defaults::VOLUME_END));
 }
--- GameEninge/Timer.hpp
+++ GameEninge/Timer.hpp
@@ -9,15 +9,36 @@
 
 namespace spic 
 {
+	/**
+	 * @brief Timer class calls given function every x milliseconds
+	*/
 	class Timer {
 	public:
+		/**
+		 * @brief Delay between every function call.
+		*/
 		typedef std::chrono::milliseconds Interval;
+
+		/**
+		 * @brief Function that is called after every interval.
+		*/
 		typedef std::function<void(void)> Timeout;
 
+		/*
+		 * @brief Destructor stops thread
+		*/
 		~Timer();
 
+		/**
+		 * @brief Starts timer.
+		 * @param interval Delay between every function call.
+		 * @param timeout Function that is called after every interval.
+		*/
 		void Start(const Interval& interval, const Timeout& timeout);
 
+		/**
+		 * @brief Stops timer
+		*/
 		void Stop() noexcept;
 	private:
 		std::unique_ptr<ThreadWrapper> threadWrapper;
